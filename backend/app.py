f022fa276ceadc2583f9b99f70f12436d21c150c
AmirthaVarshiniElavarasu <amirthavarhini@gmail.com>
2026-01-24 11:41:36 +0530

    add db

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,30 @@
 from flask import Flask
+from flask_cors import CORS
+from database import db,migrate
+from config import LocalDevelopmentConfig
+from flask_security import Security, hash_password
+from flask_restful import Api
 
 
 
 def create_app():
    app = Flask(__name__)
+   # app config
+   app.config.from_object(LocalDevelopmentConfig)
+   # database setup
+   db.init_app(app)
+   migrate.init_app(app)
+
+   CORS(app,supports_credentials=True)
+
+   with app.app_context():
+      setup_database(app)
+
+def setup_database(app):
+   #Create all tables
+   db.create_all()
+   
+
    
 
 
